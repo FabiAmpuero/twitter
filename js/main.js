bcbb5b43343c6aafa3f49731a3c3b454b0898bfa
Fabiola <fabi.ampuero.chalco@gmail.com>
2017-01-23 07:50:00 -0500

    se agrego line-through

--- js/main.js
+++ js/main.js
@@ -1,4 +1,69 @@
-var boton=document.getElementById("btn-add");
+var boton=document.getElementById("boton");
+var texto=document.getElementById("texto");
+var lista=document.getElementById("lista");
+
+boton.addEventListener("click",onButtonClick);
+
+function onButtonClick(evento){
+    agregarMensaje();
+}
+
+function agregarMensaje(){
+    var mensaje=texto.value;
+    var item=document.createElement("li");
+    item.className="tarea";
+    
+    check=document.createElement("input");
+    span=document.createElement("span");
+    var icon=document.createElement("i");
+    check.setAttribute("type","checkbox");
+    check.className="check";
+    icon.className="glyphicon glyphicon-trash tacho";
+    
+    span.innerHTML=mensaje;
+    item.appendChild(check);
+    item.appendChild(span);
+    item.appendChild(icon);
+    lista.appendChild(item);
+    
+    texto.value="";
+    texto.focus();
+    
+    check.addEventListener("click", onChange);
+}
+
+check.addEventListener("click", onChange);
+
+function onChange(){
+    span.style.textDecoration="none";
+    if(span.style.textDecoration==="none"){
+        span.style.textDecoration="line-through";
+    } else{
+        span.style.textDecoration="none";
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*var boton=document.getElementById("boton");
 boton.onclick=function(){
     var tarea=document.getElementById("texto");
     if (tarea.value == null || tarea.value.length == 0 || /^\s*$/.test(tarea.value)){
@@ -10,6 +75,6 @@ boton.onclick=function(){
         var nuevaTarea=document.getElementById("new");
         nuevaTarea.innerHTML=tarea.value;
         tarea.value="";
-        /*crear elementos al vuelo*/
+        crear elementos al vuelo
     }
-}
+}*/
